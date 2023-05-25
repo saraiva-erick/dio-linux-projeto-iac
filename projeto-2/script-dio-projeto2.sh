a7c3d6d8604fda8324a0f6fb17e280d15328469c
saraiva-erick <127536402+saraiva-erick@users.noreply.github.com>
2023-05-24 23:04:08 -0300

    Update script-dio-projeto2.sh

--- projeto-2/script-dio-projeto2.sh
+++ projeto-2/script-dio-projeto2.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Atualizando o servidor"
-apt-get update
-apt-get upgrade -y
-apt-get install apache2 -y
-apt-get install unzip -y
-apt-get install wget -y
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install apache2 -y
+sudo apt-get install unzip -y
+sudo apt-get install wget -y
 
 echo "Baixando arquivo da aplicação"
 cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
-unzip main.zip
+sudo wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+sudo unzip main.zip
 cd linux-site-dio-main
-cp -R * /var/www/html
+sudo cp -R * /var/www/html
 
 echo "Termino do script"
